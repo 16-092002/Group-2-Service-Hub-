b4064508c8bf69f77ef8609285f2299338a014d4
Kuljeet0007 <Kuljeetsingh0977@conestogac.on.ca>
2025-09-17 02:05:09 +0000

    Initial Setup and added server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/auth', require('./routes/authRoutes'));
+app.use('/ping', (req, res) => res.json({ message: 'Backend is alive!' }));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
